1f7c33574ca2d612e311c994ab8470107d786cd5
Vice <Vice@38686702-15cf-42e4-a595-3071df8bf5ea>
2008-01-08 16:45:09 +0000

    European Currencies

--- QLNet/Currencies/Currency.cs
+++ QLNet/Currencies/Currency.cs
@@ -58,5 +58,105 @@ namespace QLNet
       public Currency()
       {
       }
+
+      #region Get/Set
+      /// <summary>
+      /// currency name, e.g, "U.S. Dollar"
+      /// </summary>
+      public string name
+      {
+         get { return _data.name ; }
+      }
+      /// <summary>
+      /// ISO 4217 three-letter code, e.g, "USD"
+      /// </summary>
+      public string code
+      {
+         get { return _data.code; }
+      }
+      /// <summary>
+      /// ISO 4217 numeric code, e.g, "840"
+      /// </summary>
+      public int numericCode
+      {
+         get { return _data.numeric; }
+      }
+      /// <summary>
+      /// symbol, e.g, "$"
+      /// </summary>
+      public string Symbol
+      {
+         get { return _data.symbol; }
+      }
+      /// <summary>
+      /// fraction symbol, e.g, "¢"
+      /// </summary>
+      public string fractionSymbol
+      {
+         get { return _data.fractionSymbol; }
+      }
+      /// <summary>
+      /// number of fractionary parts in a unit, e.g, 100
+      /// </summary>
+      public int fractionsPerUnit
+      {
+         get { return _data.fractionsPerUnit; }
+      }
+      /// <summary>
+      /// rounding convention
+      /// </summary>
+      public Rounding rounding
+      {
+         get { return _data.rounding; }
+      }
+      /// <summary>
+      /// output format
+      /// The format will be fed three positional parameters,
+      /// namely, value, code, and symbol, in this order.
+      /// </summary>
+      public string format
+      {
+         get { return _data.formatString; }
+      }
+      /// <summary>
+      /// currency used for triangulated exchange when required
+      /// </summary>
+      public Currency triangulationCurrency
+      {
+         get { return _data.triangulated; }
+      }
+      #endregion
+
+      #region Methods
+      /// <summary>
+      /// is this a usable instance?
+      /// </summary>
+      /// <returns></returns>
+      public bool empty()
+      {
+         return (_data.name == string.Empty); 
+      }
+
+      public static bool operator ==(Currency c1, Currency c2)
+      {
+         return (c1.name == c2.name);
+      }
+
+      public static bool operator !=(Currency c1, Currency c2)
+      {
+         return !(c1.name == c2.name);
+      }
+
+      public override string ToString()
+      {
+         if (!this.empty())
+            return this.code;
+        else
+            return "null currency";
+      }
+
+      #endregion
+
+
    }
 }
