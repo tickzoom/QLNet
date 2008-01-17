c5ed2b88851f4abbea5a228640e62bde992f2791
Vice <Vice@38686702-15cf-42e4-a595-3071df8bf5ea>
2008-01-17 11:47:38 +0000

    ExchangeRate Tests

--- QLNet/QLNet/Math/GlobalMath.cs
+++ QLNet/QLNet/Math/GlobalMath.cs
@@ -23,6 +23,7 @@ using System.Text;
 
 namespace QLNet.Global
 {
+   
    public static class Math
    {
       public static bool close(double x, double  y)
@@ -33,10 +34,41 @@ namespace QLNet.Global
       public static bool close(double x, double y, int n)
       {
          double diff = System.Math.Abs(x - y);
-         double tolerance = n * Double.Epsilon;
+         double tolerance = n * 2.22045e-016; // double.Epsilon;
          // FLOATING_POINT_EXCEPTION
          return diff <= tolerance * System.Math.Abs(x) && diff <= tolerance * System.Math.Abs(y);
       }
+
+      public static bool close(Money m1, Money m2)
+      {
+         return close(m1, m2, 42);
+      }
+
+      public static bool close(Money m1, Money m2, int n) 
+      {
+         if (m1.currency == m2.currency) 
+         {
+            return close(m1.value,m2.value,n);
+         } 
+         else if (Money.conversionType == Money.ConversionType.BaseCurrencyConversion) 
+         {
+            Money tmp1 = m1;
+            Money.convertToBase(ref tmp1);
+            Money tmp2 = m2;
+            Money.convertToBase(ref tmp2);
+            return close(tmp1,tmp2,n);
+         } 
+         else if (Money.conversionType == Money.ConversionType.AutomatedConversion) 
+         {
+            Money tmp = m2;
+            Money.convertTo(ref tmp, m1.currency);
+            return close(m1,tmp,n);
+         } 
+         else 
+         {
+            throw new Exception("currency mismatch and no conversion specified");
+         }
+    }
 
    }
 }
