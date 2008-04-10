c809db414202daadfb38119806f1c8a07c4c2aac
sudgupta <sudgupta@38686702-15cf-42e4-a595-3071df8bf5ea>
2008-04-10 05:46:02 +0000

    solver 1D coding and UT done for bisection, secant,falseposition and ridder

--- QLNet/QLNet/Math/Solvers1d/FalsePosition.cs
+++ QLNet/QLNet/Math/Solvers1d/FalsePosition.cs
@@ -8,7 +8,7 @@
  copy of the license along with this program; if not, license is  
  available online at <http://trac2.assembla.com/QLNet/wiki/License>.
   
- QLNet is a based on QuantLib, a free-software/open-source library
+ QLNet  is a based on QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
  The QuantLib license is available online at http://quantlib.org/license.shtml.
  
