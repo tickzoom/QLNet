f44ff7b98e8e8e241a4994e43de49296229b6b66
amaggiulli <amaggiulli@38686702-15cf-42e4-a595-3071df8bf5ea>
2009-04-20 17:38:39 +0000

    QSilver - Client Application for QLNet - Initial Version

---
+++ QSilver/QSilver/Shell.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using QSilver.Resources;
+using Microsoft.Practices.Composite.UnityExtensions;
+using Microsoft.Practices.Unity;
+
+namespace QSilver
+{
+    public partial class Shell : UserControl,IShellView
+    {
+        public Shell()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowView()
+        {
+            Application.Current.RootVisual = this;
+            var story = (Storyboard)this.Resources[ResourceNames.EntryStoryboardName];
+            story.Begin();
+        }
+
+    }
+}
---
+++ QSilver/QSilver/ShellPresenter.cs
@@ -0,0 +1,14 @@
+﻿
+namespace QSilver
+{
+    public class ShellPresenter
+    {
+        public ShellPresenter(IShellView view)
+        {
+            View = view;
+
+        }
+
+        public IShellView View { get; private set; }
+    }
+}
