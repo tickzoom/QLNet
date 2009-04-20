512f08ca6bfd98e9239edaf3c1b368b2ec6b5ffd
amaggiulli <amaggiulli@38686702-15cf-42e4-a595-3071df8bf5ea>
2009-04-20 17:35:33 +0000

    QSilver - Client Application for QLNet - Initial Version This client is develop in SilverLight 2.0 and WPF . The 2 projects are linked to reuse common classes. Based on Reference App of Composite Application Guidance for WPF and Silverlight - February 2009

---
+++ QSilver/Silverlight/QSilver.Infrastructure.Silverlight/Interfaces/IHeaderInfoProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace QSilver.Infrastructure.Interfaces
+{
+    /// <summary>
+    /// Provides an easy way to recognize a class that exposes a HeaderInfo that can be used to bind 
+    /// to a header from XAML.
+    /// </summary>
+    /// <typeparam name="T">The HeaderInfo type</typeparam>
+    public interface IHeaderInfoProvider<T>
+    {
+        T HeaderInfo { get; }
+    }
+}
