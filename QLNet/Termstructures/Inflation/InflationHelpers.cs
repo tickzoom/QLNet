6ececfccc2ff44ea2c4a3ed6cb9875a16fb731e7
phreal <phreal@38686702-15cf-42e4-a595-3071df8bf5ea>
2010-04-13 00:45:45 +0000

    Add : YearOnYearInflationSwapHelper

--- QLNet/Termstructures/Inflation/InflationHelpers.cs
+++ QLNet/Termstructures/Inflation/InflationHelpers.cs
@@ -1,6 +1,7 @@
 ﻿/*
  Copyright (C) 2008, 2009 , 2010  Andrea Maggiulli (a.maggiulli@gmail.com)
-  
+ Copyright (C) 2010 Philippe Real (ph_real@hotmail.com)
+
  This file is part of QLNet Project http://www.qlnet.org
 
  QLNet is free software: you can redistribute it and/or modify it
@@ -129,4 +130,131 @@ namespace QLNet
        protected ZeroInflationIndex zii_;
        protected ZeroCouponInflationSwap zciis_;
     }
+
+    public class YearOnYearInflationSwapHelper : BootstrapHelper<YoYInflationTermStructure>
+    {
+        public YearOnYearInflationSwapHelper(
+             Handle<Quote> quote,
+             Period swapObsLag,   // lag on swap observation of index
+             Date maturity,
+             Calendar calendar,   // index may have null calendar as valid on every day
+             BusinessDayConvention paymentConvention,
+             DayCounter dayCounter,
+             YoYInflationIndex yii)
+            : base(quote)
+        {
+            swapObsLag_ = swapObsLag;
+            maturity_ = maturity;
+            calendar_ = calendar;
+            paymentConvention_ = paymentConvention;
+            dayCounter_ = dayCounter;
+            yii_ = yii;
+
+            if (yii_.interpolated())
+            {
+                // if interpolated then simple
+                earliestDate_ = maturity_ - swapObsLag_;
+                latestDate_ = maturity_ - swapObsLag_;
+            }
+            else
+            {
+                // but if NOT interpolated then the value is valid
+                // for every day in an inflation period so you actually
+                // get an extended validity, however for curve building
+                // just put the first date because using that convention
+                // for the base date throughout
+                KeyValuePair<Date, Date> limStart = Utils.inflationPeriod(maturity_ - swapObsLag_,
+                                                                yii_.frequency());
+                earliestDate_ = limStart.Key;
+                latestDate_ = limStart.Key;
+            }
+
+            // check that the observation lag of the swap
+            // is compatible with the availability lag of the index AND
+            // it's interpolation (assuming the start day is spot)
+            if (yii_.interpolated())
+            {
+                Period pShift = new Period(yii_.frequency());
+                if ((swapObsLag_ - pShift) > yii_.availabilityLag())
+                    throw new ApplicationException(
+                           "inconsistency between swap observation of index "
+                           + swapObsLag_ +
+                           " index availability " + yii_.availabilityLag() +
+                           " index period " + pShift +
+                           " and index availability " + yii_.availabilityLag() +
+                           " need (obsLag-index period) > availLag");
+
+            }
+            Settings.registerWith(update);
+        }
+
+
+        public override void setTermStructure(YoYInflationTermStructure y)
+        {
+
+            base.setTermStructure(y);
+
+            // set up a new ZCIIS
+            // but this one does NOT own its inflation term structure
+            bool own = false;
+
+            // The effect of the new inflation term structure is
+            // felt via the effect on the inflation index
+            Handle<YoYInflationTermStructure> yyts = new Handle<YoYInflationTermStructure>(y, own);
+
+            YoYInflationIndex new_yii = yii_.clone(yyts);
+
+            // always works because tenor is always 1 year so
+            // no problem with different days-in-month
+            Date from = Settings.evaluationDate();
+            Date to = maturity_;
+            Schedule fixedSchedule = new MakeSchedule().from(from).to(to)
+                                        .withTenor(new Period(1, TimeUnit.Years))
+                                        .withConvention(BusinessDayConvention.Unadjusted)
+                                        .withCalendar(calendar_)// fixed leg gets cal from sched
+                                        .backwards()
+                                        .value();
+            Schedule yoySchedule = fixedSchedule;
+            double spread = 0.0;
+            double fixedRate = quote().link.value();
+
+            double nominal = 1000000.0;   // has to be something but doesn't matter what
+            //Date start = y.nominalTermStructure().referenceDate();
+            //yyiis_.reset(new YearOnYearInflationSwap(YearOnYearInflationSwap::Payer,
+            //                                            nominal,
+            //                                            fixedSchedule,
+            //                                            fixedRate,
+            //                                            dayCounter_,
+            //                                            yoySchedule,
+            //                                            new_yii,
+            //                                            swapObsLag_,
+            //                                            spread,
+            //                                            dayCounter_,
+            //                                            calendar_,  // inflation index does not have a calendar
+            //                                            paymentConvention_));
+
+            // Because very simple instrument only takes
+            // standard discounting swap engine.
+            //yyiis_.setPricingEngine(new DiscountingSwapEngine(y.nominalTermStructure()));
+        }
+
+        //public override double impliedQuote()
+        //{
+        //    // what does the term structure imply?
+        //    // in this case just the same value ... trivial case
+        //    // (would not be so for an inflation-linked bond)
+        //    yyiis_.recalculate();
+        //    return yyiis_.fairRate();
+        //}
+
+
+        protected Period swapObsLag_;
+        protected Date maturity_;
+        protected Calendar calendar_;
+        protected BusinessDayConvention paymentConvention_;
+        protected DayCounter dayCounter_;
+        protected YoYInflationIndex yii_;
+        //protected YearOnYearInflationSwap yyiis_;
+    }
+
 }
