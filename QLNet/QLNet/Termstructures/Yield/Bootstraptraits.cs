2c051c9651312e11835f613d26945329671ad432
amaggiulli <amaggiulli@38686702-15cf-42e4-a595-3071df8bf5ea>
2010-06-17 10:16:18 +0000

    Fix for negative rates . THIS IS THE LAST COMMIT WITH VS 2008 . From the next one we start to use VS 2010.

--- QLNet/QLNet/Termstructures/Yield/Bootstraptraits.cs
+++ QLNet/QLNet/Termstructures/Yield/Bootstraptraits.cs
@@ -28,49 +28,61 @@ namespace QLNet {
     // i.e. ForwardRate derives from InterpolatedForwardCurve which derives from ForwardRateStructure
     // and same for other traits. This should make things consistent with reliance on inheritance
     // However such inheritance can conflict with PiecewiseYieldCurve inheritance
+ 
+   public interface BootstrapTraits 
+   {
+      Date initialDate(YieldTermStructure c);     // start of curve data
+      double initialValue(YieldTermStructure c);   // value at reference date
+      bool dummyInitialValue();                    // true if the initialValue is just a dummy value
+      double initialGuess();                       // initial guess
+      double guess(YieldTermStructure c, Date d); // further guesses
+      // possible constraints based on previous values
+      double minValueAfter(int s, List<double> l);
+      double maxValueAfter(int i, List<double> data);
+      // update with new guess
+      void updateGuess(List<double> data, double discount, int i);
+      int maxIterations();                          // upper bound for convergence loop
 
-    public interface BootstrapTraits {
-        Date initialDate(YieldTermStructure c);     // start of curve data
-        double initialValue(YieldTermStructure c);   // value at reference date
-        bool dummyInitialValue();                    // true if the initialValue is just a dummy value
-        double initialGuess();                       // initial guess
-        double guess(YieldTermStructure c, Date d); // further guesses
-        // possible constraints based on previous values
-        double minValueAfter(int s, List<double> l);
-        double maxValueAfter(int i, List<double> data);
-        // update with new guess
-        void updateGuess(List<double> data, double discount, int i);
-        int maxIterations();                          // upper bound for convergence loop
+      //
+      double discountImpl(Interpolation i, double t);
+      double zeroYieldImpl(Interpolation i, double t);
+      double forwardImpl(Interpolation i, double t);
+   }
 
-        //
-        double discountImpl(Interpolation i, double t);
-        double zeroYieldImpl(Interpolation i, double t);
-        double forwardImpl(Interpolation i, double t);
-    }
+   public class Discount : BootstrapTraits 
+   {
+      const double avgRate = 0.05;
+      public Date initialDate(YieldTermStructure c) { return c.referenceDate(); }   // start of curve data
+      public double initialValue(YieldTermStructure c) { return 1; }    // value at reference date
+      public bool dummyInitialValue() { return false; }   // true if the initialValue is just a dummy value
+      public double initialGuess() { return 1.0/(1.0+avgRate*0.25); }   // initial guess
+      public double guess(YieldTermStructure c, Date d) { return c.discount(d, true); }  // further guesses
+      // possible constraints based on previous values
+      public double minValueAfter(int s, List<double> l) {
+         // replace with Epsilon
+         return Const.QL_Epsilon;
+      }
+      public double maxValueAfter(int i, List<double> data) 
+      {
+         // discount are not required to be decreasing--all bets are off.
+         // We choose as max a value very unlikely to be exceeded.
+         return 3.0;
 
-    public class Discount : BootstrapTraits {
-        public Date initialDate(YieldTermStructure c) { return c.referenceDate(); }   // start of curve data
-        public double initialValue(YieldTermStructure c) { return 1; }    // value at reference date
-        public bool dummyInitialValue() { return false; }   // true if the initialValue is just a dummy value
-        public double initialGuess() { return 0.9; }   // initial guess
-        public double guess(YieldTermStructure c, Date d) { return c.discount(d, true); }  // further guesses
-        // possible constraints based on previous values
-        public double minValueAfter(int s, List<double> l) {
-            // replace with Epsilon
-            return Const.QL_Epsilon;
-        }
-        public double maxValueAfter(int i, List<double> data) { return data[i - 1]; }
-        // update with new guess
-        public void updateGuess(List<double> data, double discount, int i) { data[i] = discount; }
-        public int maxIterations() { return 50; }   // upper bound for convergence loop
+         // discounts cannot increase
+         //return data[i - 1]; 
+      }
+      // update with new guess
+      public void updateGuess(List<double> data, double discount, int i) { data[i] = discount; }
+      public int maxIterations() { return 50; }   // upper bound for convergence loop
 
-        public double discountImpl(Interpolation i, double t) { return i.value(t, true); }
-        public double zeroYieldImpl(Interpolation i, double t) { throw new NotSupportedException(); }
-        public double forwardImpl(Interpolation i, double t) { throw new NotSupportedException(); }
-    }
+      public double discountImpl(Interpolation i, double t) { return i.value(t, true); }
+      public double zeroYieldImpl(Interpolation i, double t) { throw new NotSupportedException(); }
+      public double forwardImpl(Interpolation i, double t) { throw new NotSupportedException(); }
+   }
 
     //! Zero-curve traits
-    public class ZeroYield : BootstrapTraits {
+    public class ZeroYield : BootstrapTraits 
+    {
         const double avgRate = 0.05;
 
         public Date initialDate(YieldTermStructure c) { return c.referenceDate(); }   // start of curve data
